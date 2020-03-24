0d1fe37c865404e36d51d8582ea36f90eacf9db6
Juan Carlos <juancarlos@digitalhouse.com>
2020-03-24 19:20:04 -0300

    now using enpoint with ajax

--- js/todo-list.js
+++ js/todo-list.js
@@ -1,7 +1,7 @@
 Vue.component('todo-item', {
     props : [ 'todo' ],
     template:`<li >{{ todo.text }}
-        <button type="button" v-on:click="$emit('remove', todo.id)">Remove</button></li>`
+        <button type="button" v-on:click="$emit('complete', todo.id)">Complete</button></li>`
 });
 
 Vue.component('custom-input', {
@@ -19,10 +19,7 @@ var app = new Vue({
     el: '#app', 
     data : {
         taskAddItem : '',
-        list : [
-            { id: 1, text: 'Estudiar Vue', exist : true },
-            { id: 2, text: 'Componentes', exist : true }
-        ],
+        list : [],
         errorSpan : false,
         messageError : ''
     },
@@ -33,35 +30,47 @@ var app = new Vue({
                 this.messageError = 'Debe estar lleno';
                 return false;
             }
+            let textTask = this.taskAddItem.trim();
             this.errorSpan = false;
             this.messageError = '';
-            this.list.push(
-                { id : this.generateId(), text : this.taskAddItem, exist : true }
-            );
-            this.clearTaskForm();
+
+            this.$http.post('http://localhost:3000/tasks',
+                {
+                    text : textTask, status : 'created'
+                }
+            ).then(function (response) {
+                //console.log(this.list);
+                //console.log(response.data.data);
+                this.list.push(response.data.data);
+                //console.log(this.list);
+                this.clearTaskForm();
+            }).catch(error => console.log(error));
+            
         },
         clearTaskForm : function () {
             this.taskAddItem = '';
         },
-        generateId : function () {
-            return this.lastId() + 1;
-        },
-        lastId : function () {
-            let ids = this.list.map(function (item) {
-                return item.id;
-            });
-            return Math.max(...ids);
-        },
         showError : function (todo_item) {
             
         },
-        removeTodo (idToRemove) {
-			this.list = this.list.filter(todo => {
-				return todo.id !== idToRemove
-			})
+        completeTodo (idToComplete) {
+            //make status completed to api using axios
+            this.$http.patch('http://localhost:3000/tasks/' + idToComplete + '/complete')
+                .then(function (response) {
+                    console.log(response);
+                    this.list = this.list.filter(todo => {
+                        return todo.id !== idToComplete
+                    });
+                }).catch(error => console.log(error));
 		}
     },
     computed : {
 
     },
+    mounted () {
+        //get all task to api using axios
+        this.$http.get('http://localhost:3000/tasks').then(function (response) {
+                 this.list = response.body.data
+            }).catch(error => console.log(error));
+    }
   })
